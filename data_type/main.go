815dcda2f44eba36dab7d637cd98c1fe3575d192
alrhazes <anuuman2@gmail.com>
2025-01-27 12:40:27 +0100

    commit

--- data_type/main.go
+++ data_type/main.go
@@ -11,5 +11,8 @@ func main()  {
 	fmt.Println("Data Type Learning")
 	fmt.Println("Data Type Learning")
 	fmt.Println("Data Type Learning")
+	fmt.Println("Data Type Learning")
+	fmt.Println("Data Type Learning")
+	fmt.Println("Data Type Learning")
 	
 }
